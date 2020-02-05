e7574c0936185ccb503fd9a57298a19081eb0ea9
Andrew <dawdrew@gmail.com>
2020-02-05 12:03:59 -0500

    2/4/2020 updates

--- HW2_Optimal_script.m
+++ HW2_Optimal_script.m
@@ -1,46 +1,66 @@
 %% Andrew Rearson MECE 620.01 Intro To Optimal Design
 clc;
-clear;
+clear all;
 n = 1; %
-C = functionsHW2;
 %% Function Definition
 % given info
 interval = [-10,10];
-eps = [.1,.01,.001];
+eps = [.1 , .01, .001];
 
+% functions
 mainFunction = @(x) 0.0048 * x.^4 + 0.0329 * x.^3 - 0.1283 * x.^2 - 2.7896 * x.^1 - 9.37301;
-
+mnFn = [0.0048, 0.0329, - 0.1283, - 2.7896, - 9.37301];
+sol = fzero(mainFunction,interval)
 fplot(mainFunction,interval);
-% secondFunct = @(q) .1 * q.^3 + 0.2 * q.^2 - 6.5 * q - 5;
-% plot(secondFunct);
+
+% % secondFunct = @(q) .1 * q.^3 + 0.2 * q.^2 - 6.5 * q - 5;
+% % scFN = [.1 ,+ 0.2 , - 6.5 , - 5];
+% % fplot(secondFunct,interval);
+
+% call class and pass the function
+pt1 = functionsHW2;
+% pt2 = functionsHW2(scFn);
+pt1.func = mnFn;
+% pt2.func = scFn;
+
+%large matrix initialize
+biResults = zeros(3,4);
+TimeMatrix = zeros(4,3);
+powellResults = zeros(3);
+cubicResults = zeros(3);
+gsResults = zeros(3);
+Results = zeros(3,3,3);
 %% Bisection
 j=1;
-for eps
+for value=eps()
     tic
-    biResults(j) = C.Bisection(interval(1), interval(2), eps(j), mainFunction);
-    TimeMatric(n,j)=toc;
+    biResults(j) = pt1.Bisection(interval(1), interval(2), eps(j));
+    TimeMatrix(n,j) = toc;
+    
 end
 
 
 
 n=n + 1;
 %% Powell's Method
-
+j=1;
 
 
 
 
 n=n + 1;
 %% Cubic Search
-
+j=1;
 
 
 
 
 n=n + 1;
 %% Golden Section
+j=1;
 
 
 
-
+%% Outputs
+Results = [biResults(:,1:3);powellResults;cubicResults;gsResults];
 
