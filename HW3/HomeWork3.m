22218a37ee7fb36972f87381340a2ecfee4ef88a
dawdrew <dawdrew@gmail.com>
2020-02-18 18:29:36 -0500

    Update HomeWork3.m
    added plot for part 1

--- HW3/HomeWork3.m
+++ HW3/HomeWork3.m
@@ -5,10 +5,20 @@ mainFunction = @(x1,x2) 5 .* x1.^2 + 7 .* x2.^2 - 5 .* x1 - 10 .* x2 .* x1 + x2;
 x1INT = [-2,6];
 x2INT = [-1,4];
 INT = [x1INT , x2INT];
-fcontour(mainFunction, INT);
-zhandle.LevelList = 1;
+fc = fcontour(mainFunction, INT);
+% zhandle.LevelList = 1;
+% xlabel x1
+% ylabel x2
+% title(func2str(mainFunction))
+% grid on
+% axis equal
+LabeLine = [-3 -3 0 0 5 5 10 10 20 20 30 30 50 50 100 100];
+% fc.LevelList = LabeLine;
+
+figure(1)
+% contour(fc.XData,fc.YData, fc.ZData, 'ShowText','on');
+[M, c] = contour(fc.XData,fc.YData, fc.ZData, LabeLine, 'ShowText','on');
+title(func2str(mainFunction))
 xlabel x1
 ylabel x2
-title(func2str(mainFunction))
-grid on
-axis equal
+c.LineColor = 'black';
