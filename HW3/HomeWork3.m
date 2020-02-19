3a09c07f7359d745fdb17e359b2e41dba7732d35
Andrew <dawdrew@gmail.com>
2020-02-19 00:21:42 -0500

    Got rid of useless comments and sectioned off the actual first question of 3.1

--- HW3/HomeWork3.m
+++ HW3/HomeWork3.m
@@ -1,29 +1,21 @@
 %% Home Work #3
 
-mainFunction = @(x1,x2) 5 .* x1.^2 + 7 .* x2.^2 - 5 .* x1 - 10 .* x2 .* x1 + x2;
+mF = @(x1,x2) 5 .* x1.^2 + 7 .* x2.^2 - 5 .* x1 - 10 .* x2 .* x1 + x2;
 
 x1INT = [-2,6];
 x2INT = [-1,4];
 INT = [x1INT , x2INT];
-fc = fcontour(mainFunction, INT);
-% zhandle.LevelList = 1;
-% xlabel x1
-% ylabel x2
-% title(func2str(mainFunction))
-% grid on
-% axis equal
-LabeLine = [-3 -3 0 0 5 5 10 10 20 20 30 30 50 50 100 100];
-% fc.LevelList = LabeLine;
+fc = fcontour(mF, INT);
 
+LabeLine = [-3 -3 0 0 5 5 10 10 20 20 30 30 50 50 100 100];
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 3.1 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure(1)
-% contour(fc.XData,fc.YData, fc.ZData, 'ShowText','on');
 [M, c] = contour(fc.XData,fc.YData, fc.ZData, LabeLine, 'ShowText','on');
-% hold on
-title(func2str(mainFunction))
+title(func2str(mF))
 xlabel x1
 ylabel x2
 c.LineColor = 'black';
 grid on
 axis equal
 saveas(1, 'HW3_contour.png')
-% hold off
